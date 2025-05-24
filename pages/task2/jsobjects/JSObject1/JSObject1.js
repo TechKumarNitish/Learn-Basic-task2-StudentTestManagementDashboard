16ac1eacee47255f6a325b807320f4d7d3bac6f1
Nitish Kumar <rskban15a2015@gmail.com>
2025-05-24 05:01:07 +0000

    removed extra log on no data found

--- pages/task2/jsobjects/JSObject1/JSObject1.js
+++ pages/task2/jsobjects/JSObject1/JSObject1.js
@@ -1,10 +1,11 @@
 export default {
+	flag: false,
 	myVar1: [],
 	myVar2: {},
 	myFun1 () {
 		//	write code here
 		//	this.myVar1 = [1,2,3]
-		return class_name.selectedOptionValue;
+		return Select1.selectedOptionValue;
 	},
 	async myFun2 () {
 		//	use async-await or promises

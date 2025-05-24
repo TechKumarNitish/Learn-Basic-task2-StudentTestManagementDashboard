f4aa40f90aa78083cd9d1ae85d244158b89f22b3
Nitish Kumar <rskban15a2015@gmail.com>
2025-05-24 06:14:41 +0000

    added detailed no data found log

--- pages/task2/jsobjects/filter/filter.js
+++ pages/task2/jsobjects/filter/filter.js
@@ -1,7 +1,24 @@
 export default {
 	loaded: false,
+	filterData: {
+		schoolName:'',
+		subjectName:'',
+		className:'',
+		section:'',
+		chapterName:'',
+		testName:''
+	},
 	filterBtnHanlder: async () =>{
+		this.setFilterData();
 		await getStudentChapterTestData.run();	
 		this.loaded=true;
+	},
+	setFilterData(){
+		this.filterData.schoolName=school_name.selectedOptionValue;
+		this.filterData.subjectName=subject_name.selectedOptionValue.trim();
+		this.filterData.className=class_name.selectedOptionValue;
+		this.filterData.section=section.selectedOptionValue;
+		this.filterData.chapterName=Select1.selectedOptionValue.trim();
+		this.filterData.testName=test_name.selectedOptionValue.trim();
 	}
 }
